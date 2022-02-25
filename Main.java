6abfe715e79b2c00a3da4edee3e6d1a9dc7e9908
vinicius-dias23 <63655446+vinicius-dias23@users.noreply.github.com>
2022-02-24 22:09:36 -0300

    commit inicial

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    // int codigoAluno[] = { 1, 2, 3, 4, 5 };
+    int m[][] = new int [3][4];
+
+    m[1][1] = 5;
+    System.out.println(m[1][1]);
+  }
+}
